98e2037a35b5ea0eb261bfe5d9df6de42b1d4737
justinJSwebdev <trankyhung225@gmail.com>
2025-06-30 11:37:52 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostsController } from './routes/posts/posts.controller';
+import { PostsModule } from './routes/posts/posts.module';
+import { SharedModule } from './shared/shared.module';
+
+@Module({
+  imports: [PostsModule, SharedModule],
+  controllers: [AppController, PostsController],
+  providers: [AppService],
+})
+export class AppModule {}
